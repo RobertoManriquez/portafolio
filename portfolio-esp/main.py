f32af455ffefe7936b2f709fadbe111ab7760a03
RobertoManriquez <roberto.manriquez2009@gmail.com>
2025-04-15 01:14:11 -0400

    main.py
    Añadí guardado de datos.

--- portfolio-esp/main.py
+++ portfolio-esp/main.py
@@ -10,7 +10,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-
 # Habilidades dinámicas
 @app.route('/', methods=['POST'])
 def process_form():
@@ -20,6 +19,15 @@ def process_form():
     button_db = request.form.get('button_db')
     return render_template('index.html', button_python=button_python, button_discord=button_discord, button_html=button_html, button_db=button_db)
 
+@app.route('/feedback', methods=['POST'])
+def feedback():
+    text = request.form.get('text')
+    email = request.form.get('email')
+
+    with open('feed.txt', 'a', encoding="utf-8") as file:
+        file.write(f'Email: {email} \nComentario: {text}')
+
+    return "Feedback recibido gracias"
 
 if __name__ == "__main__":
     app.run(debug=True)
